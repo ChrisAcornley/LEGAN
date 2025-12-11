915579d1f6b3410665f8e38e86c8ebfcb4180eef
Christopher Acornley <chrisacornley@hotmail.co.uk>
2025-12-11 00:42:14 +0000

    Added Max Layer to classifier to be capable of processing Generator output

--- discriminator.py
+++ discriminator.py
@@ -1,5 +1,14 @@
 import tensorflow as tf
 
+class MaxLayer(tf.keras.layers.Layer):
+    def __init__(self):
+        super(MaxLayer, self).__init__()
+        self.beta = 1e10
+
+    def call(self, inputs):
+        x_range = tf.range(inputs.shape.as_list()[-1], dtype=inputs.dtype)
+        return tf.reduce_sum(tf.nn.softmax(inputs*self.beta) * x_range, axis=-1)
+
 class PoolingClassifier(tf.keras.Model):
     def __init__(self, encoder):
         super(PoolingClassifier, self).__init__()
@@ -9,10 +18,13 @@ class PoolingClassifier(tf.keras.Model):
         self.dense_tanh = tf.keras.layers.Dense(64, activation='tanh')
         self.dense_output = tf.keras.layers.Dense(1)
         self.model_name = ""
+        self.max_layer = MaxLayer()
 
-    def call(self, inputs, training=False):
+    def call(self, inputs, use_max_layer = False, training=False):
         x = inputs
-        if self.encoder is not None:
+        if use_max_layer:
+            x = self.max_layer(x)
+        elif self.encoder is not None:
             x = self.encoder(x)
         x = self.embedding(x, training=training)
         x = self.pooling(x, training=training)
@@ -22,13 +34,13 @@ class PoolingClassifier(tf.keras.Model):
 
 class LinearDiscriminator(PoolingClassifier):
     def __init__(self, encoder=None):
-        super(LinearDiscriminator, self).__init(encoder)
+        super(LinearDiscriminator, self).__init__(encoder)
         self.embedding = LinearReplacement(64)
 
         
 class EmbeddingDiscriminator(PoolingClassifier):
     def __init__(self, encoder=None, vocab_size=0):
-        super(EmbeddingDiscriminator, self).__init(encoder)
+        super(EmbeddingDiscriminator, self).__init__(encoder)
         self.embedding = EmbeddedLinearReplacement(vocab_size, 64)
 
 class LinearReplacement(tf.keras.layers.Layer):
